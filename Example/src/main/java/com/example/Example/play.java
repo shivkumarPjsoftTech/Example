8eb61bbed868de8b8a471d618693947a6c053df6
SHIVKUMARPATIL312 <shivkumarpatil.pjsofttech@gmail.com>
2024-04-24 14:47:35 +0530

    this is my first commit

---
+++ Example/src/main/java/com/example/Example/play.java
@@ -0,0 +1,4 @@
+package com.example.Example;
+
+public class play {
+}
---
+++ Example/src/main/java/com/example/Example/shiv.java
@@ -0,0 +1,8 @@
+package com.example.Example;
+
+public class shiv {
+    public static void main(String[] args) {
+
+    }
+
+}
